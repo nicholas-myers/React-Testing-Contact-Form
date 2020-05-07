bf5ad7247b6e7e34fac4dcf859919e0d2dc590ad
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-07 14:39:53 -0700

    coinfused with instructions test to see if the input exists after action is failing - adding state to follow the guided project setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, getByLabelText } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
@@ -7,7 +7,22 @@ test("renders App without crashing", () => {
 });
 
 test("renders the inputs without errors and types into the fields, and submits the form", () => {
-  const { getByLabelText } = render(<App />)
+
+  const { getByLabelText, getByText } = render(<App />)
+
   const firstNameInput = getByLabelText(/first name/i)
+  const lastNameInput = getByLabelText(/last name/i)
+  const emailInput = getByLabelText(/email/i)
+  const messageInput = getByLabelText(/message/i)
+
   expect(firstNameInput).toBeInTheDocument()
+  expect(lastNameInput).toBeInTheDocument()
+  expect(emailInput).toBeInTheDocument()
+  expect(messageInput).toBeInTheDocument()
+  
+  fireEvent.change(firstNameInput, { target: { value: "Nick" }})
+
+  console.log(firstNameInput.value)
+  // const myName = getByDisplayValue(/nick/i)
+  // expect(myName).toBeInTheDocument()
 })
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,9 +3,20 @@ import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
   const [data, setData] = useState();
+  // const [user, setUser] = useState({
+  //   firstName: ,
+  //   firstName: ,
+  //   firstName: ,
+  //   firstName: ,
+  // })
   const { register, errors, handleSubmit } = useForm({
     mode: "onBlur",
   });
+
+  // const captureInput = (event) => {
+  //   setData()
+  // };
+
   const onSubmit = (data) => {
     setData(data);
   };
@@ -16,6 +27,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id="firstName"
             name="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
@@ -28,6 +40,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,14 +54,18 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            id="message"
+            name="message"
+            ref={register({ required: false })}
+          />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
